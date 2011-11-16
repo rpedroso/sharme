a02394838efe33568eb000a6a9842c76045aba58
Ricardo Pedroso <rmdpedroso@gmail.com>
2011-11-16 01:39:06 +0000

    fix viewer freeze on windows

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -183,6 +183,21 @@ static void viewer_draw_image_cb(void *p)
     window->redraw();
 }
 
+static void viewer_window_realize_cb(void *p)
+{
+    pmesg(3, (char*)"viewer_window_realize_cb\n");
+    //Fl::lock();
+    draw_image_args_t *di_arg = (draw_image_args_t*) p;
+    window->size((di_arg->width > Fl::w() ? Fl::w() : di_arg->width),
+                 (di_arg->height > Fl::h() ? Fl::h() : di_arg->height));
+
+    window->position((Fl::w() - window->w())/2,
+                     (Fl::h() - window->h())/2);
+    window->label("sharme viewer");
+    window->show();
+    //Fl::unlock();
+}
+
 void viewer_disconnected_cb(void *p)
 {
     disconnected_cb((SharmeUI*) p);
@@ -309,15 +324,11 @@ static inline int viewer_codec_parse_header(SmokeCodecInfo *info,
 static inline void viewer_window_realize(int width, int height)
 {
     pmesg(3, (char*)"viewer_window_realize\n");
-    Fl::lock();
-    window->size((width > Fl::w() ? Fl::w() : width),
-                 (height > Fl::h() ? Fl::h() : height));
-
-    window->position((Fl::w() - window->w())/2,
-                     (Fl::h() - window->h())/2);
-    window->label("sharme viewer");
-    window->show();
-    Fl::unlock();
+    draw_image_args_t di_arg;
+    di_arg.width = width;
+    di_arg.height = height;
+    di_arg.data = NULL;
+    Fl::awake(viewer_window_realize_cb, &di_arg);
 }
 
 static inline void viewer_get_size(int *width, int *height)
